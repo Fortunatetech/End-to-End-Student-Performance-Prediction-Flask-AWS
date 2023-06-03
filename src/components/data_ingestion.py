b0f4a9b566714e4c9cbbbd37be77ac7943e0220b
Ayodele Ayodeji <ayodeleayodeji250@gmail.com>
2023-06-03 06:41:43 -0700

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,13 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-# from src.components.data_transformation import DataTransformation
-# from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-# #from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
-
-
+#from src.components.model_trainer import ModelTrainerConfig
+#from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -27,7 +25,6 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            # this dataset could be red from anywhere
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
@@ -42,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -55,3 +52,12 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    #modeltrainer=ModelTrainer()
+    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
